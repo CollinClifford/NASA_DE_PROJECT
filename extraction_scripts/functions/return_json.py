4319054e099eaba18c29e3f6d73318cc82347a27
collin.clifford <collin.clifford@accenture.com>
2024-01-31 09:34:24 -0800

    Cleaned up return_json function/added comments

--- extraction_scripts/functions/return_json.py
+++ extraction_scripts/functions/return_json.py
@@ -1,12 +1,12 @@
 import requests
 import logging
 from functions.return_params import return_params
-from api_keys import lat_long
 
 logging.basicConfig(filename='logs/main.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def return_json(web_service, api_key, start_date, end_date, lat, lon):
     json = {}
+    
     if web_service == 'EPIC/api/natural/date/':
         url = f'https://api.nasa.gov/EPIC/api/natural/date/{start_date}'
     elif web_service == 'EPIC/api/enhanced/date/':
@@ -14,38 +14,30 @@ def return_json(web_service, api_key, start_date, end_date, lat, lon):
     else:
         url = f"https://api.nasa.gov/{web_service}"
 
-    # Condition to properly load API parameters.
+    # Condition to apply Lattitude and Longitude if Earth API is being called.
     if web_service == 'planetary/earth/assets':
-        # for city, location in lat_long.items():
         params = return_params(web_service, start_date, '2020-01-01', api_key, lat, lon)
 
         try:
             # Returns JSON for loaded web_service.
-
             response = requests.get(url, params=params)
-
             if response.status_code == 200:
                 json = response.json()
                 return json
             else:
                 logging.error(f"HTTP response: {response.status_code}")
-
         except Exception as e:
             logging.error(f"Request error: {e}")
 
     else:
         params = return_params(web_service, start_date, end_date, api_key)
         try:
-
             # Returns JSON for loaded web_service.
-
             response = requests.get(url, params=params)
-
             if response.status_code == 200:
                 json = response.json()
                 return json
             else:
                 logging.error(f"HTTP response: {response.status_code}")
-
         except Exception as e:
             logging.error(f"Request error: {e}")
