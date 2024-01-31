00333cd33bd7b9626a70d499a42215454e38a48a
collin.clifford <collin.clifford@accenture.com>
2024-01-31 09:41:34 -0800

    Added API trigger

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-# TODO: Build Services that Trigger API Extraction Scripts
 # TODO: Build Services that Trigger Data Factory and ETL process
 # TODO: Build Services that Trigger Stored Proceduers
+
+import logging
+import subprocess
+import os
+
+logging.basicConfig(filename='logs/main.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Trigger APIs
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+api_trigger = os.path.join(current_directory, 'extraction_scripts/api_extraction.py')
+
+try:
+    subprocess.run(['python', api_trigger])
+except Exception as e:
+    logging.error(f'One or more subprocesses failed: {e}')
