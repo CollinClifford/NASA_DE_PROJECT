f88ea1d762bf98d7814745e11769aa0048177ddb
collin.clifford <collin.clifford@accenture.com>
2024-01-31 10:31:18 -0800

    Started adding database initialization scripts (WIP)

---
+++ data_factory/data_warehouse_init/sql_commands/create_table.py
@@ -0,0 +1,67 @@
+def ct_apod(cursor):
+    create_table_query = """
+    CREATE TABLE apod.apod_raw (
+    id SERIAL PRIMARY KEY,
+    service_version text,
+    media_type text,
+    hdurl text,
+    title text,
+    copyright text,
+    url text,
+    date date UNIQUE,
+    explanation text,
+    thumbnail_url text
+    )
+    """
+    
+    cursor.execute(create_table_query)
+
+def ct_neows(cursor):
+    create_table_query = """
+    CREATE TABLE neows.neows_raw (
+    id SERIAL PRIMARY KEY,
+    date date UNIQUE,
+    objects JSONB
+    )
+    """
+    
+    cursor.execute(create_table_query)
+
+def ct_cme(cursor):
+    create_table_query = """
+    CREATE TABLE donki.cme_raw (
+    id SERIAL PRIMARY KEY,
+    activityID text UNIQUE,
+    catalog text,
+    startTime date,
+    sourceLocation text,
+    activeRegionNum text,
+    link text,
+    note text,
+    instruments JSONB,
+    cmeAnalyses JSONB,
+    linkedEvents text
+    )
+    """
+
+    cursor.execute(create_table_query)
+
+def ct_cme_analysis(cursor):
+    create_table_query = """
+    CREATE TABLE donki.cme_analysis_raw (
+    id SERIAL PRIMARY KEY,
+    time21_5 date UNIQUE,
+    latitude double,
+    longitude double,
+    halfAngle double,
+    speed double,
+    type text,
+    isMostAccurate boolean,
+    associatedCMEID text,
+    note text,
+    catalog text,
+    link text
+    )
+    """
+
+    cursor.execute(create_table_query)
