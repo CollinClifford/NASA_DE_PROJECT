3c8189db07b0344972ef06c2e6937895c019bd66
collin.clifford <collin.clifford@accenture.com>
2024-01-31 09:28:37 -0800

    Cleaned up extraction script, added comments

--- extraction_scripts/api_extraction.py
+++ extraction_scripts/api_extraction.py
@@ -25,37 +25,27 @@ if __name__ == "__main__":
 
     try: 
         for web_service, unique_id in web_services.items():
-            logging.info(web_service)
-            # Calls return_json function which makes GET HTTP request and returns JSON for each web service
-
+            # Condition to add lattitude and longitude, if the API needs it.
             if web_service == 'planetary/earth/assets':
+                # Circle through the lattitude and longitude for the 10 largest cities and plugs them into the params.
                 for city, location in lat_long.items():
-                        
                     try:
                         data_json = return_json(web_service, api_key, start_date, end_date, location['lat'], location['lon'])
-                        
                     except Exception as e:
                         logging.error(f"return_json function failed: {e}")
-
-                    # Calls append_to_json function which takes the JSON and saves it to local data lake.
-
                     try:
-                        # if web_service == 'neo/rest/v1/feed':
-                            append_to_json(data_json, web_service, unique_id)
-                        # else:
-                            # for entry in data_json:
-                            #     append_to_json(entry, web_service, unique_id)
+                        append_to_json(data_json, web_service, unique_id)
                     except Exception as e:
                         logging.error(f"append_to_json function failed: {e}")
+
+            # Runs all APIs except Earth API.
             else:
                 try:
                     data_json = return_json(web_service, api_key, start_date, end_date, None, None)
                 except Exception as e:
                     logging.error(f"return_json function failed: {e}")
-
-                # Calls append_to_json function which takes the JSON and saves it to local data lake.
-
                 try:
+                    # Conditions to iterate through the JSON if it is the neows API.
                     if web_service == 'neo/rest/v1/feed':
                         append_to_json(data_json, web_service, unique_id)
                     else:
