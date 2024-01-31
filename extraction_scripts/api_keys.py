f84519c4b511b6171a0172218ada1f4a5359ec53
CollinClifford <collinclifford@ymail.com>
2024-01-30 20:47:46 -0800

    added lat/long for major cities -- earth api

--- extraction_scripts/api_keys.py
+++ extraction_scripts/api_keys.py
@@ -21,3 +21,50 @@ web_services = {
     ,'mars-photos/api/v1/rovers/spirit/photos': 'id'
     ,'planetary/earth/assets': 'id'
 }
+
+lat_long = {
+    'Tokyo': {
+        'lat': 35.6895,
+        'lon': 139.6917
+    },
+    'Delhi': {
+        'lat': 28.6139,
+        'lon': 77.2090
+    },
+    'Shanghai': {
+        'lat': 31.2304,
+        'lon': 121.4737
+    },
+    'Sao Paulo': {
+        'lat': -23.5505,
+        'lon': -46.6333
+    },
+    'Mumbai': {
+        'lat': 19.0760,
+        'lon': 72.8777
+    },
+    'Mexico City': {
+        'lat': 19.4326,
+        'lon': -99.1332
+    },
+    'Osaka': {
+        'lat': 34.6937,
+        'lon': 135.5022
+    },
+    'Cairo': {
+        'lat': 30.0444,
+        'lon': 31.2357
+    },
+    'New York City': {
+        'lat': 40.7128,
+        'lon': -74.0060
+    },
+    'Beijing': {
+        'lat': 39.9042,
+        'lon': 116.4074
+    },
+    'Bremerton': {
+        'lat': 47.5673,
+        'lon': 122.6329
+    }
+}
